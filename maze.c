d0eb8365edfb533fc116d36de7b857c3872e11dc
Yixiao Ding <147287090+Yuk11119@users.noreply.github.com>
2024-04-08 08:41:26 +0000

    improve the code

--- maze.c
+++ maze.c
@@ -2,6 +2,11 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
+
+struct node{
+    int row, col;
+}s, e, curPoint, nextPoint;
 
 bool checkArgumentValidation(char *commandArgument[])
 {
@@ -41,8 +46,21 @@ char** readFile(int rows, int cols, char *fileName) {
     return map;
 }
 
+bool nodecmp(const node a, const node b)
+{
+    if(a.col != b.col || a.row != b.row) return false;
+    else return true;
+}
+
+bool checkLocation(node x)
+{
+
+}
+
 int main(char *argv[])
 {
+    char op;
+
     // Check the validation of command line arguments
     if(!checkArgumentValidation(argv)) {
         printf("ERROR: The command line argument is not valid\n");
@@ -53,10 +71,67 @@ int main(char *argv[])
     int height = atoi(argv[3]);
 
     // Check the validation of file
-    if(!checkValidationMap(width, height, argv[1])) {
+    if(!checkValidationMap(height, width, argv[1])) {
         printf("ERROR: The map is not valid\n");
         return 0;
     }
     // If the file is valide, store in map array
-    char** map = readFile(width, height, argv[1]);
+    char** map = readFile(height, width, argv[1]);
+
+    // Find the strat point and end point.
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            if(map[i][j] == 'S') {
+                s.col = j;
+                s.row = i;
+                curPoint.row = s.row;
+                curPoint.col = s.col;
+                map[i][j] = 'X';
+            }
+            if(map[i][j] == 'E') {
+                e.col = j;
+                e.row = i;
+            }
+        }
+    }
+
+    // start game
+    while(!nodecmp(curPoint, e))
+    {
+        scanf("%c", &op);
+        op = tolower(op);
+        nextPoint.row = curPoint.row;
+        nextPoint.col = curPoint.col;
+
+        // Check the input validation
+        if(op == 'm') outputMap();
+        else if(op == 'w' || op == 'a' || op == 's' || op == 'd') {
+            if(op == 'w') nextPoint.row--;
+            else if(op == 'a') nextPoint.col--;
+            else if(op == 's') nextPoint.row++;
+            else nextPoint.col++;
+
+            if(checkLocation(nextPoint)){
+                updateMap(curPoint, nextPoint);
+            }
+            else {
+                printf("oops! This step may have hit a snag.\n");
+                printf("Please try again\n");
+            }
+        }
+        else
+        
+    }
+    
+    // Output the flag of win
+    if(nodecmp(curPoint, e)) {
+        printf("Winner Winner, Chicken dinner!\n");
+        printf("Yuk1 wish you happy every day\n");
+    }
+
+    for (int i = 0; i < width; i++) {
+        free(map[i]);
+    }
+    free(map);
+    return 0;
 }
